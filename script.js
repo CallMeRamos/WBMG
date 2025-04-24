f95e7d9beb8979e1df0fe92f6c886442d4a7e76c
r <callmeramosjr@gmail.com>
2025-04-24 12:47:20 -0600

    Test

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.play-btn').addEventListener('click', () => {
+    for (let i = 0; i < 50; i++) {
+        const confetti = document.createElement('div');
+        confetti.classList.add('confetti');
+        confetti.style.left = Math.random() * 100 + 'vw';
+        confetti.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        document.body.appendChild(confetti);
+    }
+});
